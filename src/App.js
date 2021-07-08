f196e7e8b8597ab8a73a541b51d5d9c632cf5dcd
haeyong Hahn <yong80211@gmail.com>
2021-07-08 23:34:21 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -103,9 +103,28 @@ class App extends Component {
             });
           }.bind(this)} />
         <Control onChangeMode={function(_mode) {
-          this.setState({
-            mode:_mode
-          });
+          if(_mode === 'delete') {
+            if(window.confirm('really?')) {
+              var _contents = Array.from(this.state.contents); 
+              var i = 0;
+              while(i < _contents.length) {
+                if(_contents[i].id === this.state.selected_content_id) {
+                  _contents.splice(i, 1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              });
+            }
+          } 
+          else {
+            this.setState({
+              mode:_mode
+            });
+          }
         }.bind(this)} />
         {this.getContent()}
       </div>
